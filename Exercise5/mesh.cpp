fdd09b86f14661faee82717e20fcb9a16895c37d
Khai <k-h.t@hotmail.com>
2021-05-16 18:44:27 +0200

    no double vertices

--- Exercise5/mesh.cpp
+++ Exercise5/mesh.cpp
@@ -67,7 +67,8 @@ void ConnectivityAlgorithm(Mesh& mesh){
         int c = mesh.tris[i].getC();
 
         for (int tx=0; tx<3; tx++){
-        //find t0 containing b and c but not a
+
+        //find neighbours of the triangle with index
         findTx(tx, mesh, i);
         }
 
@@ -79,7 +80,8 @@ void ConnectivityAlgorithm(Mesh& mesh){
     }
 }
 
-/* find adjacent Triangle
+/* find adjacent Triangle with common points and pushes
+ * it to it of triangle
  *
  * t0 containing b and c but not a
  * t1 containing a and c but not b
@@ -87,11 +89,10 @@ void ConnectivityAlgorithm(Mesh& mesh){
  *
  *
  *param:
- *tIndex = switch Index for point identificatiion
+ *tIndex = switch Index to find correct neighbour
  *tx= index of neighbour triangle
  *tria = triangle vector of mesh
  *t= index of initial triangle
- *countPointer = pointer to our counter in findTx
  */
 void switchTx (int tIndex, int tx, Mesh& mesh, int t){
 
@@ -106,13 +107,12 @@ void switchTx (int tIndex, int tx, Mesh& mesh, int t){
     switch(tIndex) {
         //find t0 containing b and c but not a
         case 0:
-            //loop through all vertices of the current triangle and compares them with the vertices of the initial triangle
+            //loop through all vertices of the current triangle and
+            //compares them with the vertices of the initial triangle
             for (int indV=0 ; indV <3; indV++ )
             {
-
-
                 int v = mesh.tris[tx].iv[indV];
-                // when point b or c matchess increase the counter
+                // when point b or c matches increase the counter
                 if (v == b || v== c){
                     count++;
 
@@ -124,13 +124,14 @@ void switchTx (int tIndex, int tx, Mesh& mesh, int t){
                 mesh.tris[t].it[tIndex]= tx;
             }
             break;
-        //find t with c and a no b
+        //find t1 with c and a no b
         case 1:
-        //loop through all vertices of the current triangle and compares them with the vertices of the initial triangle
+        //loop through all vertices of the current triangle
+        //and compares them with the vertices of the initial triangle
             for (int indV=0; indV <3; indV++ )
             {
                 int v = mesh.tris[tx].iv[indV];
-                // when point a or c matchess increase the counter
+                // when point a or c matches increase the counter
                 if (v == a || v== c){
                     count++;
 
@@ -142,12 +143,12 @@ void switchTx (int tIndex, int tx, Mesh& mesh, int t){
                 mesh.tris[t].it[tIndex]= tx;
             }
             break;
-        //find t with a and b no c
+        //find t2 with a and b no c
         case 2:
             for (int indV=0; indV <3; indV++ )
             {
                 int v = mesh.tris[tx].iv[indV];
-                // when point a or b matchess increase the counter
+                // when point a or b matches increase the counter
                 if (v == a || v== b){
                     count++;
 
@@ -165,8 +166,8 @@ void switchTx (int tIndex, int tx, Mesh& mesh, int t){
 
 /* find the adjacent triangle on a specific side
  *
- * tIndex= index of neighbour triangle (important for switch case)
- * tria= triangles from the mesh
+ * tIndex= index to find correct neighbour (important for switch case)
+ * mesh= the mesh
  * i = index of initial triangle
  *
  * return:indexT = Index of adjacent Triangle
@@ -175,14 +176,10 @@ void findTx (int tIndex, Mesh& mesh, int i){
     // loops again throiugh all triangles to compare them with initial triangle
     for (int tx=0; tx<mesh.tris.size(); tx++)
        {
-
            //if not the initial triangle
            if (tx != i){
-
                switchTx(tIndex, tx, mesh, i);
             }
-           // if counter reaches, which means two common points the the index will be returned
-
        }
 }
 
@@ -195,18 +192,13 @@ void CalcEdge(Mesh& mesh, int i0, int i1, int i2, int i3, int i){
     Vertex v2 = mesh.pts[i2];
     Vertex v3 = mesh.pts[i3];
 
+    Vertex ev = 1.0f/8.0f*(v0 + 3*v1 + 3*v2 + v3);
 
-    Vertex ev = 3.0f/8.0f*(v0 + v1) + 1.0f/8.0f*(v2 + v3);
+    //ev.Print();
 
-    //for (int m=0; m<mesh.pts.size(); m++){
-        //Vertex comP = mesh.pts[m];
+    mesh.pts.push_back(ev);
+    mesh.tris[i].ie[mesh.pts.size()-1];
 
-        // check if new edge vertex is duplicate of existing vertex
-        //if (ev.getCoord() != comP.getCoord()){
-            mesh.pts.push_back(ev);
-            mesh.tris[i].ie[mesh.pts.size()-1];
-        //}
-    //}
 }
 
 // loop to create new subdivision points out of existing points
@@ -215,52 +207,72 @@ void LoopSubdiv (Mesh& mesh){
     // calculate edge mask out of two adjacent triangle points
     // loop through triangles and all neighbors to find e, test if e exists
 
+
+    //loop through all triangles
     for (int i= 0; i < mesh.tris.size(); i++){
 
-        // t points 0 to 2 and t0 point 3
-        //indices of points
-        int i0 = 0;
-        int i1 = 0;
-        int i2 = 0;
-        int i3 = 0;
+        // get Vertices of current Triangle
+        int a = mesh.tris[i].getA();
+        int b = mesh.tris[i].getB();
+        int c = mesh.tris[i].getC();
+        int d;
 
-        // loop through all 3 neighbors and find fourth point index
+        // loop through all 3 neighbors and find fourth point index,
         for (int j=0; j<3; j++){
+            // vertex index of current neighbour
+            int tx = mesh.tris[i].it[j];
 
-            // change order of assignment of points of t for each "rotation" through neighbor triangles
-            if (j==0){
-                i0 = mesh.tris[i].iv[0];
-                i1 = mesh.tris[i].iv[1];
-                i2 = mesh.tris[i].iv[2];
-            } else if (j == 1){
-                i0 = mesh.tris[i].iv[1];
-                i1 = mesh.tris[i].iv[0];
-                i2 = mesh.tris[i].iv[2];
-            } else if (j == 2){
-                i0 = mesh.tris[i].iv[2];
-                i1 = mesh.tris[i].iv[0];
-                i2 = mesh.tris[i].iv[1];
-            }
-
-            int it3 = mesh.tris[i].it[j];
-            // if index of t less than index of neighbor t0 calculate edge vertices
-            //if (i<it3){
+            if(i < tx){
+                // loop through points of the neighbour
                 for (int k = 0; k<3; k++){
+                    //v = index of current point
+                    int v = mesh.tris[tx].iv[k];
 
-                    int v = mesh.tris[it3].iv[k];
+                    //current point musn't be contained in current triangle
+                    if (v != a && v != b && v != c){
+                        d = v;
 
-                    if (v != i0 && v != i1 && v != i2){
-                        i3 = v;
+                        switch (j){
+                        case 0:
+                            CalcEdge(mesh, a, b, c, d, i);
+                            break;
+                        case 1:
+                            CalcEdge(mesh, b, a, c, d, i);
+                            break;
+                        case 2:
+                           CalcEdge(mesh, c, b, a, d, i);
+                            break;
+                        }
+                       }
+                        // assign indices of e's to triangle ie
 
-                        CalcEdge(mesh, i0, i1, i2, i3, i);
-                    }
                 }
-                // if index of t greater than index of neighbor t0 get ie from t0
-            //} else {
-                //mesh.tris[i].ie[j] = mesh.tris[it3].ie[j];
-            //}
+            }
         }
-        //cout << "Test: " << i0 << "," << i1 << "," << i2 << "," << i3 << endl;
+
+
+
+        //cout << "Test: " << i0 << "," << i1 << "," << i2 << "," << i3 << endl
+
+        float x;
+        float y;
+        float z;
+
+        // calculate vector of A to B
+        //float ab[]= {b[0]-a[0] ,b[1]-a[1],b[2]-a[2]};
+
+        // getting coordinates of edge vertex
+//        x= a[0] + 0.5*ab[0];
+//        y= a[1] + 0.5*ab[1];
+//        z= a[2] + 0.5*ab[2];
+
+        Vertex newE = Vertex(x,y,z);
+
+        // push three new edge vertices into ie of corresponding triangle
+        // check if point exists yet, otherwise no push
+
+        //mesh.tris[t].ie.push_back(newE);
+        //testprint
     }
 
     // implement new points with faces into mesh to make
